1a3b73a4beba0308eef0032b9e0503b147f3aa38
jayathungek <jayathunge.work@gmail.com>
2023-10-18 16:29:35 +0100

    tweaks: update enterface data hparams

--- datasets/enterface.py
+++ datasets/enterface.py
@@ -3,11 +3,11 @@ MULTILABEL = False
 
 LABEL_MAPPINGS = ["anger", "sadness", "disgust", "surprise", "happiness", "fear"]
 NUM_LABELS = len(LABEL_MAPPINGS)
-MAX_SPEC_SEQ_LEN = 375
-SPEC_MAX_LEN = 1000 
-SAMPLING_RATE = 44100
+MAX_SPEC_SEQ_LEN = 125
+SPEC_MAX_LEN = 500 
+SAMPLING_RATE = 48000
 SPEC_WINDOW_SZ_MS = 40
 SPEC_HOP_LEN_MS   = 40
-MAX_AUDIO_TIME_SEC = 15
+MAX_AUDIO_TIME_SEC = 5
 DATA_DIR = "/root/intelpa-1/datasets/enterface database"
 FACE_MARGIN = 60
